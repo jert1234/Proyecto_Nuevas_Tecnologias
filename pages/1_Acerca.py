621e497e761de960d0e09ed3aaba9aa1ff1ec30e
JUANES <juanestebanrendontorres2005@gmail.com>
2023-10-07 00:35:25 -0500

    Se creo las 2 aplicacion inicialmente

--- pages/1_Acerca.py
+++ pages/1_Acerca.py
@@ -2,3 +2,31 @@ import streamlit as st
 
 #Diseño personalizado
 st.header("Acerca")
+import streamlit as st
+
+# Título de la aplicación
+st.title("Acerca de Mí")
+
+# Información personal
+st.header("Información Personal")
+st.write("¡Hola! Soy Juan Esteban Rendont. Soy un apasionado de la programación y la tecnología.")
+
+# Foto tuya (si tienes una)
+st.image("https://firebasestorage.googleapis.com/v0/b/evidencias-75c7d.appspot.com/o/foto%20mia.jpg?alt=media&token=fa17447e-ddfc-48a5-8a45-effd8ec58023", caption="Foto de perfil", use_column_width=True)
+
+# Información adicional
+st.header("Intereses")
+st.write("Algunos de mis intereses incluyen:")
+st.write("- Programación")
+st.write("- Ciencia de datos")
+st.write("- Desarrollo web")
+
+# Contacto
+st.header("Contacto")
+st.write("Puedes contactarme en juanestebanrendontorres2005gmail.com. También puedes encontrarme en las siguientes redes sociales:")
+
+# Enlaces a tus redes sociales (si las tienes)
+
+st.write("[GitHub](https://github.com/jert1234)")
+
+
--- pages/3_Aplicación 2.py
+++ pages/3_Aplicación 2.py
@@ -2,3 +2,29 @@ import streamlit as st
 
 # Diseño personalizado
 st.header("Aplicacón 1")
+import streamlit as st
+
+# Título de la aplicación
+st.title("To-Do List App")
+
+# Crear una lista para almacenar las tareas
+tasks = []
+
+# Campo de entrada para agregar una nueva tarea
+task_input = st.text_input("Nueva tarea:")
+
+# Botón para agregar una tarea
+if st.button("Agregar"):
+    if task_input:
+        tasks.append(task_input)
+        task_input = ""
+
+# Mostrar la lista de tareas
+if tasks:
+    st.write("## Lista de Tareas:")
+    for i, task in enumerate(tasks, start=1):
+        st.write(f"{i}. {task}")
+
+# Botón para borrar todas las tareas
+if st.button("Borrar todas las tareas"):
+    tasks = []
